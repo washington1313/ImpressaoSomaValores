cda2532c4d81f5d24c97326e1aff365e9304f584
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-27 19:47:30 -0300

    Projeto Finalizado

---
+++ ImprimirSomaValores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ImprimirSomaValores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero, segundoNumero, terceiroNumero, soma;
+
+            Console.WriteLine("Digite o primeiro número da soma: ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o segundo número da soma: ");
+            segundoNumero= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro número da soma: ");
+            terceiroNumero= int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            soma = somaValores(primeiroNumero, segundoNumero, terceiroNumero);
+            Console.WriteLine("A soma dos números foi: " + soma);
+
+            Console.ReadKey();
+        }
+
+        static int somaValores (int primeiroValor, int segundoValor, int terceiroValor)
+        {
+            int resultado = primeiroValor + segundoValor + terceiroValor;
+
+            return resultado;
+        }
+
+    }
+}
